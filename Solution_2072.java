fa089c6aac25377a40112cc35e5f4f79ae3ecb80
jimmiS2 <gyfl23242@naver.com>
2021-01-07 20:39:30 +0900

    sw Q_2027

---
+++ Solution_2072.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+ 
+class Solution_2072{
+    public static void main(String args[]) throws Exception{
+         
+        Scanner sc = new Scanner(System.in);
+        int T;
+        int total = 0;
+        int num;
+         
+        T=sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++){
+            for(int num_cnt = 0; num_cnt < 10; num_cnt++){
+                num = sc.nextInt();
+                if(num%2 != 0)
+                    total += num;
+            }
+            System.out.println("#" + test_case + " " + total); 
+            total = 0;
+        }
+    }
+}
